254592f484ec0a8853113b02a26f96e825c08c01
Sadraque <sdq.sts@gmail.com>
2017-10-13 19:46:47 -0300

    Página principal pronta

--- js/index.js
+++ js/index.js
@@ -1,1 +1,25 @@
 import '../scss/main.scss'
+
+window.addEventListener('load', (e) => {
+  const siteHeader = document.querySelector('.site__header')
+  const videoContainer = document.querySelector('.video-container')
+  const verticalRedBar = document.querySelector('.vertical-red-bar')
+
+  setStyles()
+  setAnimations()
+
+  window.addEventListener('resize', (e) => { setStyles() })
+
+  function setStyles () {
+    document.documentElement.style.setProperty('--red-bar-width', siteHeader.clientHeight + 'px')
+    videoContainer.style.height = siteHeader.clientHeight + 'px'
+  }
+
+  function setAnimations () {
+    if (verticalRedBar.classList.contains('vertical-red-bar--show')) {
+      verticalRedBar.classList.remove('vertical-red-bar--show')
+    }
+
+    verticalRedBar.classList.add('vertical-red-bar--show')
+  }
+})
